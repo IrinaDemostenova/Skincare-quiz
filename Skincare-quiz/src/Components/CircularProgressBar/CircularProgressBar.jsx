15285830e0b41961e1a8658f8421ad18029c2ac4
Irina Demostenova <i.demostenova@gmail.com>
2024-06-22 13:33:42 +0300

    Adding Circular Progress Bar Component and CSS for the Question Component

---
+++ Skincare-quiz/src/Components/CircularProgressBar/CircularProgressBar.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import './CircularProgressBar.css';
+
+const CircularProgressBar = ({ progress, id, progressText }) => {
+    const radius = 40;
+    const circumference = 2 * Math.PI * radius;
+    const strokeDashoffset = circumference * (1 - progress / 100);
+
+    return (
+        <svg width="100" height="100" id={id}>
+            <circle id='inner-circle' cx="50" cy="50" r={radius} />
+            <circle id='outer-circle' cx="50" cy="50" r={radius} strokeDasharray={circumference} strokeDashoffset={strokeDashoffset} />
+            <text x="50" y="50" textAnchor="middle" dominantBaseline="middle">{progressText}</text>
+        </svg>
+    )
+}
+
+CircularProgressBar.propTypes = {
+    progress: PropTypes.number,
+    id: PropTypes.string,
+    progressText: PropTypes.string
+}
+
+export default CircularProgressBar;
--- Skincare-quiz/src/Components/Question/Question.jsx
+++ Skincare-quiz/src/Components/Question/Question.jsx
@@ -2,24 +2,23 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import './Question.css';
 import arrow from "..//../Images/arrow.svg";
+import CircularProgressBar from '../CircularProgressBar/CircularProgressBar';
 
 const Question = ({ question, questionNumber, totalNumberOfQuestions }) => {
 
     return (<div id='question'>
-        <span>{question.title}</span>
+        <span id='question-title'>{question.title}</span>
         <div id='answers'>
             {question.answers.map((answer, index) => <button key={index}>{String.fromCharCode(97 + index)}. {answer}</button>)}
         </div>
         <div id='buttons'>
             <NavLink id='back' to={questionNumber === 1 ? '/' : `/quiz/${questionNumber - 1}`}>Back</NavLink>
-            {/* да се помисли как да се направи дота със стрелата */}
             <NavLink id='next' to={questionNumber === totalNumberOfQuestions ? '/results' : `/quiz/${questionNumber + 1}`}>
-                <div className="navlink-content">
-                    {questionNumber === totalNumberOfQuestions ? 'Discover Your Results' : `Next Question`}
-                    {questionNumber !== totalNumberOfQuestions && <img src={arrow} alt='arrow' />}
-                </div>
+                <span>{questionNumber === totalNumberOfQuestions ? 'Discover Your Results' : `Next Question`}</span>
+                {questionNumber !== totalNumberOfQuestions && <img src={arrow} alt='arrow' />}
             </NavLink>
         </div>
+        <CircularProgressBar progress={(questionNumber / totalNumberOfQuestions) * 100} id='circular-bar' progressText={`${questionNumber}/${totalNumberOfQuestions}`}/>
     </div>)
 }
 
